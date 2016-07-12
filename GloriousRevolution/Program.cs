a5693f135f48a925baf81f208ef6ebfadf8588e1
Viko20 <vanbellebvba@gmail.com>
2016-07-12 19:36:06 +0200

    Edited various functions (Combo, Harass) Added Menu + Orbwalker.

--- GloriousRevolution/Program.cs
+++ GloriousRevolution/Program.cs
@@ -12,8 +12,9 @@ namespace GloriousRevolution
         private static readonly Obj_AI_Hero Player = ObjectManager.Player;
         private static Spell Q, W, E, R;
         private const string Champ = "Viktor";
-        private static Orbwalking.Orbwalker orbwalker;
-        private static Menu firstMenu;
+        private static Orbwalking.Orbwalker _orbwalker;
+        private static Menu _firstMenu;
+        private const int MaxRangeE = 1225;
 
 
         static void Main(string[] args)
@@ -41,6 +42,38 @@ namespace GloriousRevolution
             E.SetSkillshot(espell.Delay, espell.Width, espell.MissileSpeed, false, SkillshotType.SkillshotLine);
             R.SetSkillshot(rspell.Delay, rspell.Width, rspell.MissileSpeed, false, SkillshotType.SkillshotCircle);
 
+            _firstMenu = new Menu("GloriousEvolution" + Champ, "firstMenu", true);
+
+            Menu orbwalkerMenu = new Menu("Orbwalker", "Orbwalker");
+            _orbwalker = new Orbwalking.Orbwalker(orbwalkerMenu);
+            _firstMenu.AddSubMenu(orbwalkerMenu);
+
+            Menu ts = _firstMenu.AddSubMenu(new Menu("Target Selector", "Target Selector")); ;
+            TargetSelector.AddToMenu(ts);
+
+            Menu combo = _firstMenu.AddSubMenu(new Menu("Combo", "Combo"));
+            combo.AddItem(new MenuItem("QC", "Q").SetValue(true));
+            combo.AddItem(new MenuItem("WC", "W").SetValue(true));
+            combo.AddItem(new MenuItem("EC", "E").SetValue(true));
+            combo.AddItem(new MenuItem("RC", "R").SetValue(true));
+            combo.AddItem(new MenuItem("RMC", "use R on x enemies").SetValue(new Slider(2, 1, 5)));
+
+            Menu harass = _firstMenu.AddSubMenu(new Menu("Harass", "Harass"));
+            harass.AddItem(new MenuItem("QH", "Q").SetValue(true));
+            harass.AddItem(new MenuItem("EH", "E").SetValue(true));
+            harass.AddItem(new MenuItem("ManaH", "Min mana harass").SetValue(new Slider(40, 0, 100)));
+
+            Menu extra = _firstMenu.AddSubMenu(new Menu("Extra", "Extra"));
+            extra.AddItem(new MenuItem("AGCW", "Gapclose W").SetValue(true));
+            extra.AddItem(new MenuItem("IR", "Interrupt R").SetValue(true));
+            extra.AddItem(new MenuItem("CCW", "Chain stun W").SetValue(true));
+
+            Menu draw = _firstMenu.AddSubMenu(new Menu("Drawing", "Drawing"));
+            draw.AddItem(new MenuItem("DLH", "Draw Last Hit").SetValue(true));
+            draw.AddItem(new MenuItem("DK", "Draw Killable").SetValue(true));
+
+            _firstMenu.AddToMainMenu();
+
             Game.OnUpdate += Game_OnUpdate;
             Drawing.OnDraw += Drawing_Ondraw;
 
@@ -50,32 +83,106 @@ namespace GloriousRevolution
 
         private static void Game_OnUpdate(EventArgs args)
         {
-            throw new NotImplementedException();
+            if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
+                Combo();
+            if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed)
+                Harass();
         }
+
+        private static void Combo()
+        {
+
+            if (E.IsReady() && _firstMenu.Item("EC").GetValue<bool>())
+            {
+                var target = TargetSelector.GetTarget(MaxRangeE, TargetSelector.DamageType.Magical);
+
+                if (target != null)
+                    E.Cast(target);
+            }
+
+            if (Q.IsReady() && _firstMenu.Item("QC").GetValue<bool>())
+            {
+                var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
+
+                if (target != null)
+                    Q.Cast(target);
+            }
+
+            if (W.IsReady() && _firstMenu.Item("WC").GetValue<bool>())
+            {
+                var target = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Magical);
+
+                if (target != null)
+                    W.Cast(target);
+                
+            }
+
+            if (R.IsReady() && _firstMenu.Item("RC").GetValue<bool>())
+            {
+                var target = TargetSelector.GetTarget(R.Range, TargetSelector.DamageType.Magical);
+
+                if (target != null)
+                    R.Cast(target);
+
+            }
+
+        }
+
+        private static void Harass()
+        {
+            if (Player.ManaPercent >= _firstMenu.Item("ManaH").GetValue<Slider>().Value)
+            {
+                if (E.IsReady() && _firstMenu.Item("EC").GetValue<bool>())
+                {
+                    var target = TargetSelector.GetTarget(MaxRangeE, TargetSelector.DamageType.Magical);
+
+                    if (target != null)
+                        E.Cast(target);
+                }
+
+                if (Q.IsReady() && _firstMenu.Item("QC").GetValue<bool>())
+                {
+                    var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
+
+                    if (target != null)
+                        Q.Cast(target);
+                }
+            }
+        }
+
 
         private static void Drawing_Ondraw(EventArgs args)
         {
-            foreach (Obj_AI_Hero enemy in HeroManager.Enemies)
+            if (Player.IsDead)
+                return;
+
+            if (_firstMenu.Item("DK").GetValue<bool>())
             {
-                if (enemy.IsVisible && !enemy.IsDead && IsKillable())
+                foreach (var enemy in HeroManager.Enemies)
                 {
-                    Drawing.DrawText(enemy.HPBarPosition.X, enemy.HPBarPosition.Y + 50, Color.Red, "Killable");
+                    if (enemy.IsVisible && !enemy.IsDead && IsKillable())
+                    {
+                        Drawing.DrawText(enemy.HPBarPosition.X, enemy.HPBarPosition.Y + 50, Color.Red, "Killable");
+                    }
                 }
             }
 
-            foreach (Obj_AI_Minion minion in MinionManager.GetMinions(1500))
+            if (_firstMenu.Item("DK").GetValue<bool>())
             {
-                if (!minion.IsDead && minion.IsVisible && !minion.IsAlly)
+                foreach (Obj_AI_Minion minion in MinionManager.GetMinions(1500))
                 {
-                    double damage = Damage.GetAutoAttackDamage(Player, minion, false);
-                    if (damage > minion.Health)
+                    if (!minion.IsDead && minion.IsVisible && !minion.IsAlly)
                     {
-                        Render.Circle.DrawCircle(minion.Position, minion.BoundingRadius + 10, Color.Red);
+                        var damage = Player.GetAutoAttackDamage(minion, false);
+                        if (damage > minion.Health)
+                        {
+                            Render.Circle.DrawCircle(minion.Position, minion.BoundingRadius + 10, Color.Red);
+                        }
                     }
                 }
-                
             }
         }
+
 
 
         public static bool IsKillable()
